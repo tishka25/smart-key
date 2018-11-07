391163177adcb51c10adbffc5ba1bfee4f28033c
Tedko <teodorstanishev@gmail.com>
2018-11-07 23:07:51 +0200

    asd

---
+++ platforms/ios/www/plugins/cordova-plugin-native-transitions/www/nativetransitions.js
@@ -0,0 +1,34 @@
+cordova.define("cordova-plugin-native-transitions.NativeTransitions", function(require, exports, module) {
+
+var exec = require("cordova/exec");
+
+var NativeTransitions = function () {
+    this.name = "NativeTransitions";
+};
+
+NativeTransitions.prototype.flip = function (duration, direction, onComplete) {
+    var options = {
+        "duration": duration,
+        "direction": direction
+    };
+    exec(onComplete, null, "NativeTransitions", "flip", [options]);
+};
+
+NativeTransitions.prototype.curl = function (duration, direction, onComplete) {
+    var options = {
+        "duration": duration,
+        "direction": direction
+    };
+    exec(onComplete, null, "NativeTransitions", "curl", [options]);
+};
+
+NativeTransitions.prototype.fade = function (duration, onComplete) {
+    var options = {
+        "duration": duration
+    };
+    exec(onComplete, null, "NativeTransitions", "fade", [options]);
+};
+
+module.exports = new NativeTransitions();
+
+});
